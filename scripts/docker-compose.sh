2f20633a6b6d512483986fbf1437ee6a8cef3fc2
Benjamin Eidelman <beneidel@gmail.com>
2015-07-11 22:28:52 -0300

    silent curl

--- scripts/docker-compose.sh
+++ scripts/docker-compose.sh
@@ -4,7 +4,7 @@ set -e
 
 if ! which docker-compose >/dev/null; then
   echo installing docker-compose…
-  sudo curl -L https://github.com/docker/compose/releases/download/1.3.1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
+  sudo curl -s -L https://github.com/docker/compose/releases/download/1.3.1/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
   sudo chmod +x /usr/local/bin/docker-compose
   echo docker-compose installed
 fi
